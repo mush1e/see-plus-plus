accd12ff7287621344de68f90edd4a88876d39b4
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-06-27 00:28:30 -0400

    [task] Added Event Loop Definition

--- src/reactor/event_loop.hpp
+++ src/reactor/event_loop.hpp
@@ -2,7 +2,12 @@
 
 #include "notifier.hpp"
 #include "../executor/thread_pool.hpp"
+#include "../core/router.hpp"
+#include "../core/types.hpp"
 
+#include <mutex>
+#include <atomic>
+#include <map>
 namespace REACTOR {
     static constexpr uint32_t FLAG_READ       = 1;  // Flag for Readable    
     static constexpr uint32_t FLAG_DISCONNECT = 2;  // Flag for EOF/Closed
@@ -10,8 +15,30 @@ namespace REACTOR {
 
     class EventLoop {
     public:
-        EventLoop(EXECUTOR::ThreadPool* thread_pool);
-        
+        EventLoop(EXECUTOR::ThreadPool& thread_pool, CORE::Router &router);
+        ~EventLoop();
+
+        bool setup_server_socket(uint16_t port);
+        void run();        
+        void stop();
+    
+    private:
+        void handle_event(const EventData& event);
+        void handle_new_connections();
+        void handle_client_event(int fd, uint32_t events);
+        void handle_client_disconnect(int fd);
+        int make_socket_nonblocking(int socket_fd);
+
+        std::unique_ptr<EventNotifier> notifier;
+        EXECUTOR::ThreadPool* thread_pool;
+        CORE::Router &router;
+        int server_socket {};
+        std::atomic<bool> should_stop {};
+
+        // Manage active connections
+        std::map<int, std::shared_ptr<CORE::ConnectionState>> connections_;
+        std::mutex connections_mutex_;
+
     };
 
 } // namespacr REACTOR
